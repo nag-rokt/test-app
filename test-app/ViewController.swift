e93fc5f32d3d3bb8cd11fe9cd3f71099033f1693
Nahuel Alan Grimaudo <v-ngrimaudo@mparticle.com>
2025-05-13 16:06:15 -0300

    Initial Commit

---
+++ test-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-app
+//
+//  Created by Nahuel Local Account on 13/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test-appTests/test_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_appTests.swift
+//  test-appTests
+//
+//  Created by Nahuel Local Account on 13/05/2025.
+//
+
+import Testing
+@testable import test_app
+
+struct test_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
